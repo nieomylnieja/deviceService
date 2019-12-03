87770c97cccd721f267a67d113609da6a91a33fc
nieomylnieja <matihawrus@gmail.com>
2019-12-03 14:40:47 +0100

    Initial commit

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/gorilla/mux v1.7.3
+	github.com/influxdata/influxdb-client-go v0.1.4
 	github.com/stretchr/testify v1.4.0
 	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
 	github.com/xdg/stringprep v1.0.0 // indirect
--- writerService.go
+++ writerService.go
@@ -2,10 +2,21 @@ package main
 
 import (
 	"fmt"
+	influxdb "github.com/influxdata/influxdb-client-go"
 	"io"
 )
 
 type MeasurementsWriterService struct{}
+
+func (m MeasurementsWriterService) Coonect() error {
+	// You can generate a Token from the "Tokens Tab" in the UI
+	influx, err := influxdb.New(http: //127.0.0.1:9999, myToken, influxdb.WithHTTPClient(myHTTPClient))
+	if err != nil {
+		panic(err) // error handling here; normally we wouldn't use fmt but it works for the example
+	}
+	// Add your app code here
+	influx.Close() // closes the client.  After this the client is useless.
+}
 
 func (m MeasurementsWriterService) Start(publish <-chan Measurement, w io.Writer) error {
 	var err error
